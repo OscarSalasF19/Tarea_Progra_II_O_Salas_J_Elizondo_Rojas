0d1630b6bb9d17524d668048e54f37949f052196
Juniel <juan.elizondo.garcia@est.una.ac.cr>
2025-04-12 19:55:01 -0600

    mini commit para empezar a programar el añadir deportes

---
+++ TournamentManager/src/main/java/cr/ac/una/tournamentmanager/Controller/SportsManagerController.java
@@ -0,0 +1,19 @@
+package cr.ac.una.tournamentmanager.Controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class SportsManagerController extends Controller implements Initializable {
+
+    @Override
+    public void initialize() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
